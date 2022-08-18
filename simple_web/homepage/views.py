4bf3e00e61dfd9f580615f9b72089189600576a1
Jose Apablaza <japablaza@gmail.com>
2022-08-18 17:12:23 -0500

    Import num of salas

--- simple_web/homepage/views.py
+++ simple_web/homepage/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import time
 
+from salas.models import salas
+
+num_salas = {'numero': salas.objects.count()}
+
 def inicio (request):
-    return render(request, "simple_web/welcome.html")
+    return render(request, "simple_web/welcome.html",
+    num_salas)
 
 def sobremi (request):
     return HttpResponse("This page is about me")
@@ -11,4 +16,4 @@ def sobremi (request):
 # def lahora(request):
 #     return HttpResponse(f'This is the time in CR: '+ time.strftime('%a %s: %Z\n'))
 def lahora (request):
-    return render(request, "salas/salas.html")
+    return render(request, "salas/salas.html", num_salas)
