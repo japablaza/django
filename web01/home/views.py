032d775073da562b77d77cf958de2b523c04239b
Pepe <38828734+japablaza@users.noreply.github.com>
2023-08-03 10:41:26 -0500

    Update views.py (comments)

--- web01/home/views.py
+++ web01/home/views.py
@@ -4,9 +4,10 @@ from datetime import datetime
 
 # Create your views here.
 
-
+# Simple message
 def inicio(request):
     return HttpResponse("This is the main page, Bienvenidos!", {"message": "Hola Tulio!!!"})
 
+# Get the current time
 def lahora(request):
     return(HttpResponse(f"La fecha y hora actual: {datetime.now()}"))
