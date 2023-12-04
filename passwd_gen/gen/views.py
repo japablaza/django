3b8aae863494f0ee7f9e35fdc400c863fc8cf128
Jose Apablaza <japablaza@gmail.com>
2023-12-04 12:41:44 -0600

    Render and HttpResponse

--- passwd_gen/gen/views.py
+++ passwd_gen/gen/views.py
@@ -4,7 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'gen/home.html', {'tag': 'palabra'})
+    return render(request, 'gen/home.html')
 
 def about(request):
-    return HttpResponse('About the author')
+    return render(request, 'gen/about.html', {'Extra':'Data'})
+
+def page1(request):
+    return HttpResponse('Simpe page')
+
+def passwd(request):
+    return HttpResponse('CLAVE')
