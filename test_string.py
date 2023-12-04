020d1acd23f7baa05881354243f434474fb40a0f
Jose Apablaza <japablaza@gmail.com>
2023-12-04 14:57:34 -0600

    Simple script to test the string library

---
+++ test_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import string
+
+abc = string.ascii_lowercase
+num = list(range(0,10))
+
+print(abc)
+print(type(abc))
+
+abecedario = list(abc)
+
+print(abecedario)
+print(type(abecedario))
+
+print(num)
+print(type(num))
