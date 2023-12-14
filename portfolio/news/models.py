085ba73f774d8b438a756ff32220eb277eebd908
Jose Apablaza <japablaza@gmail.com>
2023-12-14 05:24:15 -0600

    url

--- portfolio/news/models.py
+++ portfolio/news/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Project(models.Model):
+class News(models.Model):
     titulo = models.CharField(max_length=50)
     texto = models.CharField(max_length=300)
     image = models.ImageField(upload_to='news/images/')
-
+    url = models.URLField(blank=True)
